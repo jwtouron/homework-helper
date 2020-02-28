43713f0de292c5d52b3b259449ee1dd0a7521be2
John Touron <john.touron@root9b.com>
2020-02-28 12:58:10 -0600

    Multiple improvements

--- src/SquareRoots.elm
+++ src/SquareRoots.elm
@@ -1,11 +1,15 @@
 module SquareRoots exposing (Msg(..), main, update, view)
 
 import Browser
+import Browser.Dom as Dom
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode as Json
 import Maybe
 import Random
+import Round
+import Task
 
 
 main =
@@ -21,28 +25,33 @@ main =
 -- Model
 
 
-type Status
+type SubmitStatus
+    = Correct
+    | Incorrect
+    | Imprecise
+
+
+type CompleteStatus
     = Complete
     | Incomplete
-    | Correct
-    | Incorrect
 
 
 type alias Model =
     { firstNum : Int
     , secondNum : Int
-    , status : Status
     , firstInput : String
     , secondInput : String
     , attempted : Int
     , successful : Int
+    , completeStatus : CompleteStatus
+    , submitStatus : Maybe SubmitStatus
     }
 
 
 newNums : Random.Generator ( Int, Int )
 newNums =
     Random.map2 (\a b -> ( a, b ))
-        (Random.uniform 1 [ 2, 3, 4, 5, 6 ])
+        (Random.uniform 1 [ 2, 3, 4, 5, 6, 7, 8 ])
         (Random.uniform 1 [ 2, 3, 5, 7 ])
 
 
@@ -50,11 +59,12 @@ init : () -> ( Model, Cmd Msg )
 init _ =
     ( { firstNum = 0
       , secondNum = 0
-      , status = Incomplete
       , firstInput = ""
       , secondInput = ""
       , attempted = 0
       , successful = 0
+      , completeStatus = Incomplete
+      , submitStatus = Nothing
       }
     , Random.generate RandNums newNums
     )
@@ -66,50 +76,88 @@ init _ =
 
 type Msg
     = RandNums ( Int, Int )
-    | Submit
+    | SubmitButton
     | ChangeFirst String
     | ChangeSecond String
     | Continue
+    | SubmitKeyDown Int
+    | NoOp
+
+
+checkSubmission : Model -> SubmitStatus
+checkSubmission model =
+    let
+        first =
+            Maybe.withDefault 1 (String.toInt model.firstInput)
+
+        second =
+            Maybe.withDefault 1 (String.toInt model.secondInput)
+    in
+    if first == model.firstNum && second == model.secondNum then
+        Correct
+
+    else if first * first * second == model.firstNum * model.firstNum * model.secondNum then
+        Imprecise
+
+    else
+        Incorrect
+
+
+isComplete : Model -> Bool
+isComplete model =
+    toFloat model.successful
+        / toFloat model.attempted
+        >= 0.9
+        && toFloat model.attempted
+        >= 12
+
+
+submit : Model -> ( Model, Cmd Msg )
+submit model =
+    let
+        submitStatus =
+            checkSubmission model
+
+        newModel =
+            { model | submitStatus = Just submitStatus, attempted = model.attempted + 1 }
+    in
+    case submitStatus of
+        Correct ->
+            let
+                newModel2 =
+                    { newModel | successful = newModel.successful + 1 }
+            in
+            if isComplete newModel2 then
+                ( { newModel2 | completeStatus = Complete }, Cmd.none )
+
+            else
+                ( newModel2, Cmd.none )
+
+        Incorrect ->
+            ( newModel, Cmd.none )
+
+        Imprecise ->
+            ( { newModel | attempted = newModel.attempted - 1 }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        SubmitKeyDown key ->
+            if key == 13 then
+                submit model
+
+            else
+                ( model, Cmd.none )
+
         RandNums ( x, y ) ->
             ( { model | firstNum = x, secondNum = y }, Cmd.none )
 
-        Submit ->
-            if
-                Maybe.withDefault 1 (String.toInt model.firstInput)
-                    == model.firstNum
-                    && Maybe.withDefault 1 (String.toInt model.secondInput)
-                    == model.secondNum
-            then
-                let
-                    model2 =
-                        { model
-                            | attempted = model.attempted + 1
-                            , successful = model.successful + 1
-                            , status = Correct
-                        }
-
-                    status2 =
-                        if
-                            toFloat model2.successful
-                                / toFloat model2.attempted
-                                >= 0.9
-                                && model2.attempted
-                                >= 10
-                        then
-                            Complete
-
-                        else
-                            Correct
-                in
-                ( { model2 | status = status2 }, Cmd.none )
-
-            else
-                ( { model | attempted = model.attempted + 1, status = Incorrect }, Cmd.none )
+        SubmitButton ->
+            submit model
 
         ChangeFirst x ->
             ( { model | firstInput = x }, Cmd.none )
@@ -118,7 +166,12 @@ update msg model =
             ( { model | secondInput = x }, Cmd.none )
 
         Continue ->
-            ( { model | status = Incomplete, firstInput = "", secondInput = "" }
+            ( { model
+                | completeStatus = Incomplete
+                , submitStatus = Nothing
+                , firstInput = ""
+                , secondInput = ""
+              }
             , Random.generate RandNums newNums
             )
 
@@ -136,89 +189,147 @@ subscriptions model =
 -- View
 
 
-showProblem : Model -> List (Html Msg)
-showProblem model =
-    [ h2 [] [ text "Simplify the following:" ]
-    , h3 [] [ text ("√" ++ String.fromInt (model.firstNum * model.firstNum * model.secondNum)) ]
-    , span []
-        [ label [] [ text "Answer:" ]
-        , input [ value model.firstInput, onInput ChangeFirst, disabled (model.status /= Incomplete) ] []
-        , label [] [ text "√" ]
-        , input [ value model.secondInput, onInput ChangeSecond, disabled (model.status /= Incomplete) ] []
-        , button [ onClick Submit, disabled (model.status /= Incomplete) ] [ text "Submit" ]
+onKeyDown : (Int -> msg) -> Attribute msg
+onKeyDown tagger =
+    on "keydown" (Json.map tagger keyCode)
+
+
+shouldDisableInput : Model -> Bool
+shouldDisableInput model =
+    case model.submitStatus of
+        Just Imprecise ->
+            False
+
+        Nothing ->
+            False
+
+        _ ->
+            True
+
+
+problemView : Model -> Html Msg
+problemView model =
+    div []
+        [ h4 [ class "subtitle is-4" ] [ text "Simplify the following:" ]
+        , h4 [ class "title is-4" ] [ text ("√" ++ String.fromInt (model.firstNum * model.firstNum * model.secondNum)) ]
+        , div [ class "field is-grouped" ]
+            [ label [ class "label is-large" ] [ text "Answer:" ]
+            , input
+                [ class "input"
+                , style "max-width" "50px"
+                , value model.firstInput
+                , onInput ChangeFirst
+                , disabled (shouldDisableInput model)
+                , onKeyDown SubmitKeyDown
+                ]
+                []
+            , label [ class "label is-large", style "margin-left" "7px" ] [ text " × √" ]
+            , input
+                [ class "input"
+                , style "max-width" "50px"
+                , value model.secondInput
+                , onInput ChangeSecond
+                , disabled (shouldDisableInput model)
+                , onKeyDown SubmitKeyDown
+                ]
+                []
+            , button
+                [ class "button is-primary"
+                , onClick SubmitButton
+                , disabled (shouldDisableInput model)
+                ]
+                [ text "Submit" ]
+            ]
         ]
-    ]
+
+
+statsView : Model -> Html Msg
+statsView model =
+    h3 [ class "subtitle is-3" ]
+        [ text
+            ("Attempted: "
+                ++ String.fromInt model.attempted
+                ++ " Successful: "
+                ++ String.fromInt model.successful
+                ++ " ("
+                ++ (if model.attempted == 0 then
+                        "0.00%)"
+
+                    else
+                        Round.round 2 (toFloat model.successful / toFloat model.attempted * 100)
+                            ++ "%)"
+                   )
+            )
+        ]
+
+
+explanationView : Model -> Html Msg
+explanationView model =
+    div []
+        [ h2 [ class "title is-2" ] [ text "Incorrect!" ]
+        , label [ class "label is-large" ] [ text ("√" ++ String.fromInt (model.firstNum * model.firstNum * model.secondNum)) ]
+        , label [ class "label is-large" ]
+            [ text
+                ("= √("
+                    ++ String.fromInt (model.firstNum * model.firstNum)
+                    ++ " × "
+                    ++ String.fromInt model.secondNum
+                    ++ ")"
+                )
+            ]
+        , label [ class "label is-large" ]
+            [ text
+                ("= √"
+                    ++ String.fromInt (model.firstNum * model.firstNum)
+                    ++ " × √"
+                    ++ String.fromInt model.secondNum
+                )
+            ]
+        , label [ class "label is-large" ]
+            [ text
+                ("= "
+                    ++ String.fromInt model.firstNum
+                    ++ " × √"
+                    ++ String.fromInt model.secondNum
+                )
+            ]
+        , button [ class "button is-primary", onClick Continue ] [ text "Continue" ]
+        ]
 
 
 view : Model -> Html Msg
 view model =
     div []
-        ([ h1 [] [ text "Square Roots" ]
-         , h2 []
-            [ text
-                ("Attempted: "
-                    ++ String.fromInt model.attempted
-                    ++ " Successful: "
-                    ++ String.fromInt model.successful
-                    ++ " ("
-                    ++ (if model.attempted == 0 then
-                            "0%)"
+        [ section [ class "section" ]
+            [ div [ class "container" ] [ h1 [ class "title is-1 is-spaced" ] [ text "Square Roots" ] ]
+            , statsView model
+            , case ( model.submitStatus, model.completeStatus ) of
+                ( _, Complete ) ->
+                    h2 [ class "title is-2" ] [ text "Complete!" ]
 
-                        else
-                            String.fromFloat (toFloat model.successful / toFloat model.attempted * 100)
-                                ++ "%)"
-                       )
-                )
+                ( Nothing, Incomplete ) ->
+                    problemView model
+
+                ( Just Imprecise, _ ) ->
+                    div []
+                        [ problemView model
+                        , h2 [ class "title is-2" ] [ text "Imprecise!" ]
+                        , h4 [ class "subtitle is-4" ] [ text "There is a more precise way to simplify this problem, try again." ]
+                        ]
+
+                ( Just Correct, _ ) ->
+                    div []
+                        [ problemView model
+                        , div []
+                            [ h2 [ class "title is-2" ] [ text "Correct!" ]
+                            , button [ class "button is-primary", onClick Continue ] [ text "Continue" ]
+                            ]
+                        ]
+
+                ( Just Incorrect, _ ) ->
+                    div []
+                        [ problemView model
+                        , explanationView model
+                        ]
             ]
-         ]
-            ++ (case model.status of
-                    Complete ->
-                        [ h2 [] [ text "Complete!" ] ]
-
-                    Incomplete ->
-                        showProblem model
-
-                    Correct ->
-                        showProblem model
-                            ++ [ div []
-                                    [ h2 [] [ text "Correct!" ]
-                                    , button [ onClick Continue ] [ text "Continue" ]
-                                    ]
-                               ]
-
-                    Incorrect ->
-                        showProblem model
-                            ++ [ div []
-                                    [ h2 [] [ text "Incorrect!" ]
-                                    , text ("√" ++ String.fromInt (model.firstNum * model.firstNum * model.secondNum))
-                                    , div []
-                                        [ text
-                                            ("= √("
-                                                ++ String.fromInt (model.firstNum * model.firstNum)
-                                                ++ " * "
-                                                ++ String.fromInt model.secondNum
-                                                ++ ")"
-                                            )
-                                        ]
-                                    , div []
-                                        [ text
-                                            ("= √"
-                                                ++ String.fromInt (model.firstNum * model.firstNum)
-                                                ++ " * √"
-                                                ++ String.fromInt model.secondNum
-                                            )
-                                        ]
-                                    , div []
-                                        [ text
-                                            ("= "
-                                                ++ String.fromInt model.firstNum
-                                                ++ " * √"
-                                                ++ String.fromInt model.secondNum
-                                            )
-                                        ]
-                                    , h2 [] []
-                                    , button [ onClick Continue ] [ text "Continue" ]
-                                    ]
-                               ]
-               )
-        )
+        ]
